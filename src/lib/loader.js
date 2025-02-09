4257b7bb49b4d3be24b54dc94e7e2c1144249852
neurolabusc <rorden@sc.edu>
2025-02-09 07:25:56 -0500

    use nifti-reader-js toArrayBuffer()

--- src/lib/loader.js
+++ src/lib/loader.js
@@ -1,126 +1,6 @@
 import readVox from "vox-reader"
 import * as nifti from "nifti-reader-js";
 
-function toArrayBuffer(hdr, includeExtensions = false) {
-  const SHORT_SIZE = 2;
-  const FLOAT32_SIZE = 4;
-  let byteSize = 348 + 4; // + 4 for the extension bytes
-
-  // calculate necessary size
-  if (includeExtensions) {
-    for (let extension of hdr.extensions) {
-      byteSize += extension.esize;
-    }
-  }
-  let byteArray = new Uint8Array(byteSize);
-  let view = new DataView(byteArray.buffer);
-  // sizeof_hdr
-  view.setInt32(0, 348, hdr.littleEndian);
-
-  // data_type, db_name, extents, session_error, regular are not used
-
-  // dim_info
-  view.setUint8(39, hdr.dim_info);
-
-  // dims
-  for (let i = 0; i < 8; i++) {
-    view.setUint16(40 + SHORT_SIZE * i, hdr.dims[i], hdr.littleEndian);
-  }
-
-  // intent_p1, intent_p2, intent_p3
-  view.setFloat32(56, hdr.intent_p1, hdr.littleEndian);
-  view.setFloat32(60, hdr.intent_p2, hdr.littleEndian);
-  view.setFloat32(64, hdr.intent_p3, hdr.littleEndian);
-
-  // intent_code, datatype, bitpix, slice_start
-  view.setInt16(68, hdr.intent_code, hdr.littleEndian);
-  view.setInt16(70, hdr.datatypeCode, hdr.littleEndian);
-  view.setInt16(72, hdr.numBitsPerVoxel, hdr.littleEndian);
-  view.setInt16(74, hdr.slice_start, hdr.littleEndian);
-
-  // pixdim[8], vox_offset, scl_slope, scl_inter
-  for (let i = 0; i < 8; i++) {
-    view.setFloat32(
-      76 + FLOAT32_SIZE * i,
-      hdr.pixDims[i],
-      hdr.littleEndian
-    );
-  }
-  view.setFloat32(108, hdr.vox_offset, hdr.littleEndian);
-  view.setFloat32(112, hdr.scl_slope, hdr.littleEndian);
-  view.setFloat32(116, hdr.scl_inter, hdr.littleEndian);
-
-  // slice_end
-  view.setInt16(120, hdr.slice_end, hdr.littleEndian);
-
-  // slice_code, xyzt_units
-  view.setUint8(122, hdr.slice_code);
-  view.setUint8(123, hdr.xyzt_units);
-
-  // cal_max, cal_min, slice_duration, toffset
-  view.setFloat32(124, hdr.cal_max, hdr.littleEndian);
-  view.setFloat32(128, hdr.cal_min, hdr.littleEndian);
-  view.setFloat32(132, hdr.slice_duration, hdr.littleEndian);
-  view.setFloat32(136, hdr.toffset, hdr.littleEndian);
-
-  // glmax, glmin are unused
-
-  // descrip and aux_file
-  byteArray.set(new TextEncoder().encode(hdr.description), 148);
-  byteArray.set(new TextEncoder().encode(hdr.aux_file), 228);
-
-  // qform_code, sform_code
-  view.setInt16(252, hdr.qform_code, hdr.littleEndian);
-  view.setInt16(254, hdr.sform_code, hdr.littleEndian);
-
-  // quatern_b, quatern_c, quatern_d, qoffset_x, qoffset_y, qoffset_z, srow_x[4], srow_y[4], and srow_z[4]
-  view.setFloat32(256, hdr.quatern_b, hdr.littleEndian);
-  view.setFloat32(260, hdr.quatern_c, hdr.littleEndian);
-  view.setFloat32(264, hdr.quatern_d, hdr.littleEndian);
-  view.setFloat32(268, hdr.qoffset_x, hdr.littleEndian);
-  view.setFloat32(272, hdr.qoffset_y, hdr.littleEndian);
-  view.setFloat32(276, hdr.qoffset_z, hdr.littleEndian);
-  const flattened = hdr.affine.flat();
-  // we only want the first three rows
-  for (let i = 0; i < 12; i++) {
-    view.setFloat32(
-      280 + FLOAT32_SIZE * i,
-      flattened[i],
-      hdr.littleEndian
-    );
-  }
-  // intent_name and magic
-  byteArray.set(new TextEncoder().encode(hdr.intent_name), 328);
-  byteArray.set(new TextEncoder().encode(hdr.magic), 344);
-  // add our extension data
-  if (includeExtensions) {
-    byteArray.set(Uint8Array.from([1, 0, 0, 0]), 348);
-    let extensionByteIndex = hdr.getExtensionLocation();
-    for (const extension of hdr.extensions) {
-      view.setInt32(
-      extensionByteIndex,
-      extension.esize,
-      extension.littleEndian
-      );
-      view.setInt32(
-      extensionByteIndex + 4,
-      extension.ecode,
-      extension.littleEndian
-      );
-      byteArray.set(
-      new Uint8Array(extension.edata),
-      extensionByteIndex + 8
-      );
-      extensionByteIndex += extension.esize;
-    }
-    } else {
-    // In a .nii file, these 4 bytes will always be present
-    byteArray.set(new Uint8Array(4).fill(0), 348);
-  }
-
-  return byteArray.buffer;
-}
-
 export async function vox2nii(inBuffer, isVerbose = true) {
   try {
     let byteArray;
@@ -174,7 +54,6 @@ export async function vox2nii(inBuffer, isVerbose = true) {
     hdr.littleEndian = true
     hdr.magic = "n+1"
     const hdrBuffer = hdr.toArrayBuffer()
-    //const hdrBuffer = toArrayBuffer(hdr)
     // Merge header and voxel data
     const niftiData = new Uint8Array(hdrBuffer.byteLength + voxelData.byteLength);
     niftiData.set(new Uint8Array(hdrBuffer), 0);
